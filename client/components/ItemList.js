47b779749e0583ee228e18b7dc9f1240f09cab53
KamoMitoyan <kamo.mitoyan.y@tumo.org>
2021-07-28 18:05:20 +0400

    Updated ItemList

--- client/components/ItemList.js
+++ client/components/ItemList.js
@@ -7,12 +7,13 @@ function ItemList(props) {
         items{
             id
             name
+            quantity
         }
     }
     `,props.order);
 
     const OrderItems = items.map((item,index) => {
-       
+        
             return <div className="container">
                 <li key={item.id} className="item">
                     <div className="RightPart">
@@ -23,7 +24,7 @@ function ItemList(props) {
                         </div>
                         <div className="itemsNames">
                             <h4 className="item_place">item {index + 1} out of {items.length}-</h4>
-                            <h4 className="item_name">{item.name}</h4>
+                            <h4 className="item_name">{item.quantity} {item.name}</h4>
                         </div>
                         <div className = "ingredients">
                             <h4>INGREDIENTS</h4>
