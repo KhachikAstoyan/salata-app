20fa7165a98600ad8f6b5001c983b31ea495f955
Raffi Hovagimian <raffi.hovagimian@gmail.com>
2021-07-30 15:28:59 +0400

    Fix merge problem

--- client/components/OrderComponent.js
+++ client/components/OrderComponent.js
@@ -1,56 +1,85 @@
-//@ts-check
+// @ts-check
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+// @ts-ignore
 import { graphql, useFragment } from "react-relay";
-import ItemList from "./ItemList";
+import ItemList from "../components/ItemList";
 
 
 function OrderComponent(props) {
-    const [showItems, setShowItems] = useState(false);
-
+    const [showItem, toogleItem] = useState(false);
     const order = useFragment(graphql`
     fragment OrderComponent_order on OrderType{
         ...ItemList_order
-
+        id
+        client {
+            name
+        }
+        orderNumber
+        items{
+            id
+        }
+        dueTime
+        status
     }
-    `, props.order);
-    const [selected, setSelected] = useState("1");
+    `,props.order);
+    const [selected, setSelected] = useState(order.status);
     let className = "";
-    if (selected === "1") {
-        className = "option1"
-    } else if (selected === "2") {
+    if (selected === "NOT_STARTED") {
+        className = "option1";
+    } else if (selected === "IN_PROGRESS") {
         className = "option2"
-    }
-
-    else if (selected === "3") {
+    } else if (selected === "COMPLETED") {
         className = "option3"
     }
 
-    return (
-        <div>
-            <div className="ordersList">
-                <div className="order">
-                    <div className="icon">
-                        <i onClick={() => setShowItems(!showItems)} style={{ cursor: "pointer" }} className="material-icons">{showItems ? "expand_more" : "chevron_right"}</i>
-                    </div>
-                    <div className="orderInfo">
-                        <div className="orderID">Order #23651325 - 2 items</div>
-                        <div className="orderName">For Khachik Astoyan</div>
-                    </div>
-                    <div className="dueDate">Due by 12:00 pm</div>
-                    <div className="input-field col s12">
-                        <select className={`browser-default ${className}`} onChange={(event) => {
-                            setSelected(event.target.value);
-                        }} defaultValue="1">
-                            <option value="1" >Not started</option>
-                            <option value="2" >Completed</option>
-                            <option value="3" >In progress</option>
-                        </select>
-                    </div>
-                </div>
-                {showItems && <ItemList order={order} />}
-            </div>
 
+    let clientName = "";
+    let orderNumber = "";
+    let duetime;
+
+    if(order.dueTime){
+        duetime = order.dueTime;
+    }
+
+    const event2 = new Date(); 
+    event2.setTime(duetime);
+
+
+    if(order.client) {
+        clientName = order.client.name;
+    }
+    if(order.orderNumber){
+        orderNumber = order.orderNumber;
+    }
+
+
+    return (
+            <div className="orderlist-itemlist">
+        <div className="ordersList">
+                <div className="icon">
+                    <i className="material-icons" onClick={() =>{
+                        toogleItem(!showItem);
+                    }}>
+                        {!showItem ? 'chevron_right' : 'expand_more'}
+                    </i>
+                </div>
+                <div className="orderInfo">
+                    <div className="orderID">Order #{orderNumber} - {order.items.length} items</div>
+                    <div className="orderName">For {clientName}</div>
+                </div>
+                <div className="dueDate">Due by {event2.toLocaleTimeString()}</div>
+                <div className="input-field col s12">
+                    <select className={`browser-default ${className}`} onChange={(event) => {
+                        setSelected(event.target.value);
+                    }} defaultValue={selected}>
+                        <option value="NOT_STARTED" className="option1">Not Started</option>
+                        <option value="IN_PROGRESS" className="option2">In Progress</option>
+                        <option value="COMPLETED" className="option3">Completed</option>
+                    </select>
+                </div>
+                
+            </div>
+               {showItem && <ItemList order={order} />}
         </div>
     );
 
