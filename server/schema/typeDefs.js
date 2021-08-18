76281fc229f7e6a9db7c08da4ddd14cd563b3601
Michael Aghumyan <mikaelaghumyan@outlook.com>
2021-08-18 18:49:03 +0400

    add type definitions

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,79 @@
+const gql = require("graphql-tag");
+
+module.exports = gql`
+  schema {
+    query: RootQueryType
+    mutation: Mutation
+  }
+
+  type RootQueryType {
+    orders: [OrderType]
+    clientOrders(id: String!): [OrderType]
+  }
+
+  type OrderType {
+    id: String
+    client: ClientType
+    dueTime: Float
+    isTakeout: Boolean
+    items: [ItemType]
+    orderNumber: Int
+    startTime: String
+    status: StatusType
+  }
+
+  type ClientType {
+    id: ID
+    name: String
+    phoneNumber: String
+  }
+
+  type ItemType {
+    id: ID
+    ingredients: [IngredientType]
+    extra: [IngredientType]
+    additionalInformation: String
+    name: String
+    audio: AudioType
+    quantity: Int
+  }
+
+  type IngredientType {
+    id: String
+    name: String
+  }
+
+  type AudioType {
+    data: String
+  }
+
+  enum StatusType {
+    NOT_STARTED
+    IN_PROGRESS
+    COMPLETED
+  }
+
+  type Mutation {
+    addOrder(input: input): OrderType
+    addItems(
+      orderId: ID
+      ingredients: [ID]
+      extra: String
+      additionalInformation: String
+      name: String
+      quantity: Int
+    ): OrderType
+    addIngredients(id: String, name: String, items: [ID]): OrderType
+    updateOrderStatus(id: String, status: StatusType): OrderType
+    addClient(name: String, phoneNumber: String): ClientType
+  }
+
+  input input {
+    clientId: String
+    dueTime: String
+    isTakeout: Boolean
+    itemsId: [ID]
+    startTime: String
+    status: StatusType
+  }
+`;
