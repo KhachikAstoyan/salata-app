c92f089df99962f5f7179bc893948cacabf34eef
Michael Aghumyan <mikaelaghumyan@outlook.com>
2021-08-25 20:47:28 +0400

    remove ingredientResolver

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,23 +1,17 @@
 const clientResolvers = require("./ClientType");
 const orderResolvers = require("./OrderType");
-const itemResolvers = require("./ClientType");
-const ingredientResolvers = require("./IngredientType");
 
 module.exports = {
   RootQueryType: {
     ...clientResolvers.Query,
     ...orderResolvers.Query,
-    ...itemResolvers.Query,
-    ...ingredientResolvers.Query,
   },
   Mutation: {
     ...clientResolvers.Mutation,
     ...orderResolvers.Mutation,
-    ...itemResolvers.Mutation,
-    ...ingredientResolvers.Mutation,
   },
   OrderType: { ...orderResolvers.OrderType },
-  ItemType: { ...itemResolvers.ItemType },
+  OrderedItemType: { ...orderResolvers.OrderedItemType },
   StatusType: {
     NOT_STARTED: 0,
     IN_PROGRESS: 1,
