764ce266d94c7a5faae53741ed49e6797f9857b2
Michael Aghumyan <mikaelaghumyan@outlook.com>
2021-09-13 17:51:01 +0400

    create AddOrder page

---
+++ client/src/components/pages/AddOrder.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const AddOrder = () => {
+  return (
+    <main className="container max-w-5xl mx-auto">
+      <div className="order">
+        <h2 className="text-4xl">Salat</h2>
+      </div>
+    </main>
+  );
+};
+
+export default AddOrder;
