26da34e26d0347e259fb5981b71933f7f62ab850
Michael Aghumyan <mikaelaghumyan@outlook.com>
2021-09-13 20:05:15 +0400

    create ingredient component

--- client/src/components/pages/AddOrder.js
+++ client/src/components/pages/AddOrder.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
+import Ingredient from "../Ingredient";
 
 const AddOrder = () => {
   return (
-    <main className="container max-w-5xl mx-auto">
-      <div className="order">
+    <main className="container  max-w-5xl mx-auto">
+      <div className="order block">
         <h2 className="text-4xl">Salat</h2>
+        <div className="mt-5 mb-2">
+          <h2 className="text-xl mb-2">Vegetables</h2>
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+          <Ingredient />
+        </div>
       </div>
     </main>
   );
