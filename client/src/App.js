8acf46a3ea7eb488c94c2bc835d80f2bf2db04f1
Michael Aghumyan <mikaelaghumyan@outlook.com>
2021-09-13 17:50:38 +0400

    add routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import OrderList from "./components/OrderList";
+import AddOrder from "./components/pages/AddOrder";
 
 function App() {
   return (
-    <div className="App font-Comfortaa">
-      <Header pageName="Orders" />
-      <OrderList />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/orders">
+          <div className="App font-Comfortaa">
+            <Header pageName="Orders" />
+            <OrderList />
+          </div>
+        </Route>
+        <Route path="/addOrder">
+          <Header pageName="Add Order" />
+          <AddOrder />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
