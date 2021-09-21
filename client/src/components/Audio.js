ca4caaa2ce6c7b2005ac703cf95f07470ec348c7
Arayik-Tshagharyan <arayik.t.y@gmail.com>
2021-09-21 12:44:10 +0400

    change photos of AddOrder page

--- client/src/components/Audio.js
+++ client/src/components/Audio.js
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Dropdown } from "./Button";
-import { ChevronLeft } from "./Icons.js";
-import { RewindButton } from "./Icons.js";
-import { ForwardButton } from "./Icons.js";
-import { PlayButton, PauseButton } from "./Icons.js";
+import { PlayButton, PauseButton, ForwardButton, RewindButton} from "./Icons.js";
 import audioSrc from '../Loco Ringtone _ VIRAL BGM.mp3'
 
 const audio = new Audio(audioSrc);
 
 
 const Audiolist= () => {
-    const [showContent, updateContent] = useState(false);
     const [isPlayingAudio, setIsPlayingAudio] = useState(false);
 
     useEffect(() => {
@@ -21,21 +16,19 @@ const Audiolist= () => {
 
     const handlePauseButtonClick = () => {
         setIsPlayingAudio(false);
-
         audio.pause()
 
     }
 
     const handlePlayButtonClick = () => {
         setIsPlayingAudio(true)
-
         audio.play()
 
     }
 
     return (
         <main className="container max-w-5xl mx-auto ">
-            <div className=" grid-cols-3 items-center flex ">
+            <div className="grid-cols-3 items-center flex ">
                 <RewindButton />
                 {isPlayingAudio ? <PauseButton onClick={handlePauseButtonClick} /> : <PlayButton onClick={handlePlayButtonClick} />}
                 <ForwardButton />
