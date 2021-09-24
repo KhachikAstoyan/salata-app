decb2123c4cc6940b23c3edab878541a90f8b38d
KamoMitoyan <kamo.mitoyan.y@tumo.org>
2021-09-24 18:10:04 +0400

    orders page and add orders page UI

--- client/src/components/Ingredient.js
+++ client/src/components/Ingredient.js
@@ -9,10 +9,10 @@ function Ingredient({ ingredient, checked, updateIngredient }) {
           setSelected(!selected);
           updateIngredient(ingredient.id, !selected);
         }}
-        className={`inline-flex transition duration-200 ease-in-out text-center flex-col rounded-xl border-2 mb-2 px-3 py-2 mr-2 border-primary-myGreen  ${
+        className={`inline-flex transition mr-2 duration-200 ease-in-out text-center flex-col rounded-xl border mb-2 px-3 py-2 mr-2 border-primary-myGreen  ${
           checked.indexOf(ingredient.id) > -1 &&
           "border-primary-myGreen bg-primary-itemsBg "
-        }`}
+          }`}
       >
         <img
           src={`/static/${ingredient.category.category}/${ingredient.name}.png`}
@@ -23,8 +23,9 @@ function Ingredient({ ingredient, checked, updateIngredient }) {
             e.target.src = "/static/harvest.png";
           }}
         />
-        <p className="text-txtColor">{ingredient.name}</p>
+
       </div>
+      <p className="text-txtColor break-normal ml-3 w-16 text-center text-xs">{ingredient.name}</p>
     </div>
   );
 }
