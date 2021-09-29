65e5af01133e7a1816ea1cb7bb971c747e16200d
Arayik-Tshagharyan <arayik.t.y@gmail.com>
2021-09-30 01:19:38 +0400

    add correct due date

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -172,7 +172,21 @@ const PlayButton = (props) => {
   );
 };
 
-
+const PlusButton = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6" fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 4v16m8-8H4" />
+    </svg>
+  );
+};
 
 export {
   ChevronRight,
@@ -185,4 +199,5 @@ export {
   ForwardButton,
   PauseButton,
   PlayButton,
+  PlusButton,
 };
