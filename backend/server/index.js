585d84a8222d8d073ae26997c69258951050fa70
Michael Aghumyan <mikaelaghumyan@outlook.com>
2021-10-08 00:57:56 +0400

    add subscriptionServer

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,26 +1,57 @@
-const { ApolloServer } = require("apollo-server-express");
+const { createServer } = require("http");
+const { execute, subscribe } = require("graphql");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
 
 const connect = require("./db");
-const PORT = process.env.PORT || 4000;
 
-const typeDefs = require("./schema/typeDefs");
 const resolvers = require("./schema/resolvers");
+const typeDefs = require("./schema/typeDefs");
 
-async function start() {
+(async function () {
+  connect();
+})();
+(async function () {
   const app = express();
-  const server = new ApolloServer({ typeDefs, resolvers });
+
+  const httpServer = createServer(app);
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  const subscriptionServer = SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: server.graphqlPath }
+  );
 
   await server.start();
-  await server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
   app.use(express.static(__dirname + "/static"));
   app.use(cors({ origin: "*" }));
 
-  app.listen(process.env.PORT || 4000);
-}
-
-connect().then(async function () {
-  await start();
-});
+  const PORT = 4000 || process.env.PORT;
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}/graphql`)
+  );
+})();
